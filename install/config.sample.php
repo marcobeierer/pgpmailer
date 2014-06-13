c60c51d120c84238cbd87953ca414d1ff5739261
marco <marco@webguerilla.net>
2014-06-13 12:26:13 +0200

    changed sample configuration

--- install/config.sample.php
+++ install/config.sample.php
@@ -1,10 +1,13 @@
 <?php
-$config = new stdClass();
+use MBDev\Config;
 
-$config->debug = true;
-$config->cache = false;
+$publicKeyFilepath = PATH_PUBLIC . ''; // path to the public key file (relative from /public)
+$encryptionKeyID = ''; // id of the key used for encryption
+$messageReceiverAddress = ''; // email address of the receiver of the contact form
 
-$config->publicKeyFilepath = PATH_PUBLIC . ''; // path to the public key file (relative from /public)
-$config->messageReceiverAddress = ''; // email address of the receiver of the contact form
+$config = new \PGPMailerConfig($publicKeyFilepath, $encryptionKeyID, $messageReceiverAddress);
+
+$config->setDebugging(true);
+$config->setCaching(false);
 ?>
 
