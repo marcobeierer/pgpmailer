c10a253c0b4dc235f332fc7007afc4c512c93083
marco <marco@webguerilla.net>
2014-08-02 19:27:07 +0200

    changed config related to issue number #1

--- install/config.sample.php
+++ install/config.sample.php
@@ -1,13 +1,12 @@
 <?php
-use MBDev\Config;
+use MBDev\Config\PGPMailerConfig;
 
 $publicKeyFilepath = PATH_PUBLIC . ''; // path to the public key file (relative from /public)
 $encryptionKeyID = ''; // id of the key used for encryption
 $messageReceiverAddress = ''; // email address of the receiver of the contact form
 
-$config = new \PGPMailerConfig($publicKeyFilepath, $encryptionKeyID, $messageReceiverAddress);
+$config = new PGPMailerConfig($publicKeyFilepath, $encryptionKeyID, $messageReceiverAddress);
 
 $config->setDebugging(true);
 $config->setCaching(false);
 ?>
-
