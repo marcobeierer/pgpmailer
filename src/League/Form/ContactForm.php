5ef3bf8c46325434feb24d45387a44b3ba65053f
marco <marco@webguerilla.net>
2014-08-07 14:58:21 +0200

    Corrected format

--- src/League/Form/ContactForm.php
+++ src/League/Form/ContactForm.php
@@ -8,22 +8,22 @@ class ContactForm extends AbstractType {
 
     public function buildForm(FormBuilderInterface $builder, array $options) {
 
-$builder
-->add('name', 'text', array('label' => 'Name'))
-->add('email', 'email', array('label' => 'Email'))
-->add('subject', 'text', array('label' => 'Subject'))
-->add('message', 'textarea', array(
-'label' => 'Message',
-'attr' => array(
-'rows' => '7')))
-->add('submit', 'submit', array(
-'label' => 'Send email',
-'attr' => array(
-'class' => 'btn btn-default')));
-    }
+		$builder
+			->add('name', 'text', array('label' => 'Name'))
+			->add('email', 'email', array('label' => 'Email'))
+			->add('subject', 'text', array('label' => 'Subject'))
+			->add('message', 'textarea', array(
+				'label' => 'Message',
+				'attr' => array(
+					'rows' => '7')))
+			->add('submit', 'submit', array(
+				'label' => 'Send email',
+				'attr' => array(
+					'class' => 'btn btn-default')));
+	}
 
-public function getName() {
-return 'contact_form';
-}
+	public function getName() {
+		return 'contact_form';
+	}
 }
 ?>
